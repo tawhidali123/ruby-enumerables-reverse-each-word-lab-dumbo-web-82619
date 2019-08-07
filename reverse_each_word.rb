1c683570f216534aac64a17dbd5fab35a8fe0efe
Tawhid Ali <tawhidali126@gmail.com>
2019-08-07 15:52:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   break_down = string.split("")
   array = []
-  string.size.times
-  array.pop(break_down)
-  array.join 
+  string.size.times {
+    array.pop(break_down)
+  }
   
 end
