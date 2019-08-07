e8d4123a3e56199577edfce8a01849c663a796e1
Tawhid Ali <tawhidali126@gmail.com>
2019-08-07 15:48:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
 def reverse_each_word(string)
-  break_down = string.split("")
+ # break_down = string.split("")
+  #array = []
+  #array.pop(break_down.reverse)
+  #array.join 
+  
+  break_down = string.reverse
+  break_down.split("")
   array = []
-  array.pop(break_down.reverse)
-  array.join 
+  array.pop(break_down)
+  array.join
 end
