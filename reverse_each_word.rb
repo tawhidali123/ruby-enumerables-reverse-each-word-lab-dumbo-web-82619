600468ab289f96430df37b77919f6ec3074087ee
Tawhid Ali <tawhidali126@gmail.com>
2019-08-07 15:49:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
- # break_down = string.split("")
-  #array = []
-  #array.pop(break_down.reverse)
-  #array.join 
+  break_down = string.split("")
+  array = []
+  array.pop(break_down.reverse)
+  array.join 
   
 end
